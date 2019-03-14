497ace281a56996ac0cdb5d4def677c87077e466
freelance <freelance-computing@outlook.com>
2019-03-14 19:20:20 +0000

    added scroll listener

--- js/output.js
+++ js/output.js
@@ -39,6 +39,22 @@ $(document).ready(function () {
 	});
 });
 
+$('body').on('mousewheel DOMMouseScroll', function (e) {
+	if (typeof e.originalEvent.detail == 'number' && e.originalEvent.detail !== 0) {
+		if (e.originalEvent.detail > 0) {
+			console.log('Down');
+		} else if (e.originalEvent.detail < 0) {
+			console.log('Up');
+		}
+	} else if (typeof e.originalEvent.wheelDelta == 'number') {
+		if (e.originalEvent.wheelDelta < 0) {
+			console.log('Down');
+		} else if (e.originalEvent.wheelDelta > 0) {
+			console.log('Up');
+		}
+	}
+});
+
 /*($('[data-name="hnm"]').offset().left > $(window).width() - 60 - $('[data-name="hnm"]').width()) && ($('[data-name="hnm"]').offset().left < $(window).width() - 60)
 var child = o - featured__background
 $('[data-name="hnm"]').children(".o-featured__background").eq(0).width();*/
