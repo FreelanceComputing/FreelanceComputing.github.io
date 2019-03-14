713c52e904aa2b61ab1617a6b243c06292bb6e16
freelance <freelance-computing@outlook.com>
2019-03-14 19:40:55 +0000

    added invertir

--- js/output.js
+++ js/output.js
@@ -11,14 +11,14 @@ $(document).ready(function () {
         return inView;
     }
 
-    $(function () {
+    function invertir() {
         if (inView()) {
             $(".c-ui__logo").html("Aye");
         }
         else {
             $(".c-ui__logo").html("Nay");
         }
-    });
+    }
 
 	$(".o-featured__case").hover(function () {
 	    var dark = $(this).attr("data-isDark");
@@ -40,17 +40,18 @@ $(document).ready(function () {
 
 	$('body').on('mousewheel DOMMouseScroll', function (e) {
 		if (typeof e.originalEvent.detail == 'number' && e.originalEvent.detail !== 0) {
-			if (e.originalEvent.detail > 0) {
+			/*if (e.originalEvent.detail > 0) {
 				console.log('Down');
 			} else if (e.originalEvent.detail < 0) {
 				console.log('Up');
-			}
+			}*/
 		} else if (typeof e.originalEvent.wheelDelta == 'number') {
-			if (e.originalEvent.wheelDelta < 0) {
+			invertir();
+			/*if (e.originalEvent.wheelDelta < 0) {
 				console.log('Down');
 			} else if (e.originalEvent.wheelDelta > 0) {
 				console.log('Up');
-			}
+			}*/
 		}
 	});
 });
