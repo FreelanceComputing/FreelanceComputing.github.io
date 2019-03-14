0c1946ac833329822e29a8d48fe5fba8e8d84cbf
ba138360 <ba138360@UKST0055>
2019-03-14 10:49:44 +0000

    moved scroll functions out of document ready

--- js/output.js
+++ js/output.js
@@ -1,18 +1,6 @@
 $(document).ready(function () {
 
-	$(function caseLeft() {
-	    return $('[data-name="hnm"]').offset().left - $(window).scrollLeft();
-	});
-
-	$(function caseWidth() {
-	    return $('[data-name="hnm"]').width();
-	});
-
-	$(window).scroll(function () { //when window is scrolled
-	    $(".c-ui__logo").html("left index: <b>" + caseLeft() + "</b>px, case width: <b>" + caseWidth() + "</b>px.");
-	});
-
-	/*$(".o-featured__case").hover(function () {
+	$(".o-featured__case").hover(function () {
 	    var dark = $(this).attr("data-isDark");
 	    if (dark == "0") {
 	        $(".c-ui__nav-items.js-invert").addClass("invert");
@@ -28,5 +16,17 @@ $(document).ready(function () {
 	    else {
 	        $(".c-ui__nav-items.js-invert").removeClass("invert");
 	    }
-	});*/
+	});
+});
+
+$(function caseLeft() {
+    return $('[data-name="hnm"]').offset().left - $(window).scrollLeft();
+});
+
+$(function caseWidth() {
+    return $('[data-name="hnm"]').width();
+});
+
+$(window).scroll(function () { //when window is scrolled
+    $(".c-ui__logo").html("left index: <b>" + caseLeft() + "</b>px, case width: <b>" + caseWidth() + "</b>px.");
 });
