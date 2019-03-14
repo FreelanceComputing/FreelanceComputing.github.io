39c754de06b4f035221e76bf3f133fa494ce04d5
freelance <freelance-computing@outlook.com>
2019-03-14 22:53:54 +0000

    added getBoundingClientRect

--- js/output.js
+++ js/output.js
@@ -31,16 +31,27 @@ $(document).ready(function () {
 		var cases = $('[data-isDark="0"]');
 		for (var i = 0; i < cases.length; i++) {
 			var caseBackgrd = cases.eq(i).children(".o-featured__background").eq(0);
-			var caseWidth = caseBackgrd.width();
-			var caseRightEdgePos = caseBackgrd.offset().left + caseWidth;
-			views.push((caseRightEdgePos >= 0) && (caseRightEdgePos <= $(window).width()));
+			var rect = caseBackgrd.getBoundingClientRect();
+			var inVp = (rect.bottom >= 0 && rect.right >= 0 && rect.top <= (window.innerHeight || document.documentElement.clientHeight) && rect.left <= (window.innerWidth || document.documentElement.clientWidth));
+			views.push(inVp);
 		}
 		return views;
+
+		//var rect = el.getBoundingClientRect();
+
+		//return (
+		//	rect.top >= 0 &&
+		//	rect.left >= 0 &&
+		//	rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+		//	rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+		//);
+
+		//return (rect.bottom >= 0 && rect.right >= 0 && rect.top <= (window.innerHeight || document.documentElement.clientHeight) && rect.left <= (window.innerWidth || document.documentElement.clientWidth));
 	}
 
-	$(".o-landing__featured").on('touchmove', function (event) {
+	$(".o-featured").on('touchmove', function (event) {
 		var views = inViewM();
-		if (views.indexOf(true) > -1) {
+		if (views.indexOf(true) < 0) {
 			$(".c-ui__burger.js-invert").addClass("invert");
 		}
 		else {
