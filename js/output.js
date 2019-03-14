4d65bd034530d10dfd7a637987c924df0bccfc36
freelance <freelance-computing@outlook.com>
2019-03-14 19:26:13 +0000

    moved inside ready

--- js/output.js
+++ js/output.js
@@ -36,24 +36,25 @@ $(document).ready(function () {
 	    else {
 	        $(".c-ui__nav-items.js-invert").removeClass("invert");
 	    }
+		});
+
+	$('body').on('mousewheel DOMMouseScroll', function (e) {
+		if (typeof e.originalEvent.detail == 'number' && e.originalEvent.detail !== 0) {
+			if (e.originalEvent.detail > 0) {
+				console.log('Down');
+			} else if (e.originalEvent.detail < 0) {
+				console.log('Up');
+			}
+		} else if (typeof e.originalEvent.wheelDelta == 'number') {
+			if (e.originalEvent.wheelDelta < 0) {
+				console.log('Down');
+			} else if (e.originalEvent.wheelDelta > 0) {
+				console.log('Up');
+			}
+		}
 	});
 });
 
-$('body').on('mousewheel DOMMouseScroll', function (e) {
-	if (typeof e.originalEvent.detail == 'number' && e.originalEvent.detail !== 0) {
-		if (e.originalEvent.detail > 0) {
-			console.log('Down');
-		} else if (e.originalEvent.detail < 0) {
-			console.log('Up');
-		}
-	} else if (typeof e.originalEvent.wheelDelta == 'number') {
-		if (e.originalEvent.wheelDelta < 0) {
-			console.log('Down');
-		} else if (e.originalEvent.wheelDelta > 0) {
-			console.log('Up');
-		}
-	}
-});
 
 /*($('[data-name="hnm"]').offset().left > $(window).width() - 60 - $('[data-name="hnm"]').width()) && ($('[data-name="hnm"]').offset().left < $(window).width() - 60)
 var child = o - featured__background
